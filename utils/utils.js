f2696524a4c2a6f03787854c1f6c51b7e607d9c4
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-01-31 13:49:21 +0100

    add utils

---
+++ utils/utils.js
@@ -0,0 +1,17 @@
+//Her kan jeg lave funktioner og variabler jeg kan eksportere til js.js
+
+export function sayHi(user) {
+  console.log(`Hello ${user}`);
+}
+
+export function getRandomNumber(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export function $(selector) {
+  return document.querySelector(selector);
+}
+
+export function $$(selector) {
+  return document.querySelectorAll(selector);
+}
