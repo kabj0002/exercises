b434b0a1e57c3c40bcc46d491a486ca0744ed47e
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-02-05 08:16:37 +0100

    update bar chart

---
+++ bar_chart_starter_kit/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//Forestil dig en webside der viser det antal kunder der er i en kø lige nu
+//Den opdateres hvert sekund og holder en historik over de sidste 20 værdier
+//som et “animeret” søjlediagram hvor det nuværende antal kunder er vist i den
+//søjle der er længst til højre og så “scroller” værdien/søjlen til venstre.
+
+//Da du ikke har adgang til rigtig data må du simulere det ved at lave en funktion
+//der kan generere et random nummer mellem 0 og 100 der så vil være antallet af kunder
+//og bestemme din søjles højde.
+
+const list = document.querySelector("ul");
+
+const barArray = [];
+
+setInterval(generateBars, 1000);
+
+//generere random number
+function generateBars() {
+  const randomNum = Math.round(Math.random() * 100);
+  barArray.push(randomNum);
+  console.log("GENERATE BARS", barArray);
+
+  const li = document.createElement("li");
+  li.style.setProperty("--height", randomNum);
+  list.appendChild(li);
+
+  if (barArray.length > 20) {
+    console.log("Den er læst");
+    barArray.shift(); //Fjerner det første i arrayet
+    list.removeChild(list.firstElementChild); //sørger for at første søjle fjernes så der altid er 20 søjler
+  }
+}
